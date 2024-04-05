1b02b122ca4f5a685f4a649a5896da061f5972fb
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2024-04-05 23:06:16 +0900

    feat: Response Logger

--- src/Http/Middleware/ResponseLogger.php
+++ src/Http/Middleware/ResponseLogger.php
@@ -5,11 +5,11 @@ namespace LaravelBlinkLogger\Http\Middleware;
 use Closure;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Log\LogManager;
-use Illuminate\Support\Str;
 use Psr\Log\LoggerInterface;
 
-class RequestLogger
+class ResponseLogger
 {
     /**
      * @param Repository $config
@@ -23,8 +23,10 @@ class RequestLogger
 
     public function handle(Request $request, Closure $next)
     {
+        $response = $next($request);
+
         if ($this->isWrite($request)) {
-            $this->write($request);
+            $this->write($request, $response);
         }
 
         return $next($request);
@@ -32,12 +34,12 @@ class RequestLogger
 
     protected function isWrite(Request $request): bool
     {
-        $includePaths = $this->config->get('blink-logger.http.request.include_paths');
+        $includePaths = $this->config->get('blink-logger.http.response.include_paths');
         if (count($includePaths) > 0) {
             return in_array($request->path(), $includePaths, true);
         }
 
-        $excludePaths = $this->config->get('blink-logger.http.request.exclude_paths');
+        $excludePaths = $this->config->get('blink-logger.http.response.exclude_paths');
         if (count($excludePaths) > 0) {
             return ! in_array($request->path(), $excludePaths, true);
         }
@@ -45,12 +47,15 @@ class RequestLogger
         return true;
     }
 
-    protected function write(Request $request): void
+    protected function write(Request $request, Response $response): void
     {
-        $data = [
-            'request' => $request->all(),
-        ];
-
-        $this->logger->channel($this->config->get('blink-logger.http.request.channel'))->debug(sprintf('%s: %s', $request->method(), $request->fullUrl()), $data);
+        $this->logger->channel($this->config->get('blink-logger.http.response.channel'))->debug(sprintf(
+            '%d %s',
+            $response->status(),
+            $response->statusText(),
+        ), [
+            'body' => $response->content(),
+            'headers' => $response->headers->all(),
+        ]);
     }
 }
